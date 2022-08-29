3c2d0654bbb44994c517c0258a138a4ea61a7552
gerardojgarcia <gerardojgarcia@pm.me>
2022-08-29 08:32:36 -0700

    finished scrambler logic

--- main.js
+++ main.js
@@ -1,27 +1,113 @@
+
+
+const buttonEl = document.getElementById('button');
+const insertEl = document.getElementById('insert')
+const inputEl = document.getElementById('text')
+
+const resetEl = document.getElementById('reset')
+
+
+
+
+
+const newWord = []
+
+function inputValue() {
+    const inputValue = inputEl.value
+    input = inputValue
+    text = []
+
+    text.push(input)
+
+     const newValue = text.toString()
+
+
+     const newWordArray = newValue.split("") 
+
+    
+
+    newWord.push(newWordArray)
+
+    move()
+
+    
+  
+   
+   
+}
+
+
+ scrambled = []
+
+
+function move() {
  
- const price = [ '20', '30', '40' ]
+
+    
+    
+  const tempWord = newWord[0].slice(0,2)
+  const tempWord2 = newWord[0].slice(2,4)
+  const tempWord3 = newWord[0].slice(4, 6)
+  const tempWord4 = newWord[0].slice(6, 8)
+  const tempWord5 = newWord[0].slice(8)
+    const finalWord = tempWord2 + tempWord + tempWord3 + tempWord4 + tempWord5
+
+
+  
+
+
+ //const scrambledWord = newWord.concat(tempWord, tempWord2)
+  
  
- const buttonEl = document.getElementById('button')
- 
- 
- const insertEl = document.getElementById('insert')
+    scrambled.push(finalWord)
 
 
+    console.log(tempWord, tempWord2, tempWord3, finalWord)
 
-
-
-
-buttonEl.addEventListener('click', function() {
+    
+    return scrambled
 
    
-    prices()
 
+}
+
+
+
+function render( ){
+    random = scrambled.toString()
+    
+    
+    insertEl.innerText = random.replace(/,/g, '')
+
+    console.log(random.replace(/,/g, ''))
+    return random
+}
+
+function reset(){
+    inputEl.value = ""
+    insertEl.innerText = ''
+    window.location.reload()
+
+
+    
+}
+
+
+
+
+buttonEl.addEventListener('click', function(){
+
+    inputValue()
+
+    
+
+    render()
 })
 
 
-function prices() {
-    
-    prices = price
-    prices.push('chuki')
-    insertEl.innerText = prices
-}
+resetEl.addEventListener('click', function(){
+
+    reset()
+
+
+})
